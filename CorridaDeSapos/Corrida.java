84e742651b227a7a74858bf244964ecede91745d
Thales Davi de Souza <81756816+ThalesDaviSouza@users.noreply.github.com>
2023-03-08 16:35:27 -0300

    Finalizada a corrida de Sapos

--- CorridaDeSapos/Corrida.java
+++ CorridaDeSapos/Corrida.java
@@ -1,23 +1,19 @@
 package CorridaDeSapos;
+import java.time.chrono.ThaiBuddhistChronology;
 import java.util.*;
 
 public class Corrida {
     static final int distanciaCorrida = 100;
-    static final int numSapos = 10;
+    static final int numSapos = 5;
 
     public static void main(String[] args) {
+        Random rand = new Random();
         List<Sapo> sapos = new ArrayList<Sapo>();
-        for(int i = 0; i < numSapos; i++){
-            sapos.add(new Sapo(0, 5, i, distanciaCorrida));
+        for(int i = 1; i <= numSapos; i++){
+            sapos.add(new Sapo(0, rand.nextInt(10) + 1, i, distanciaCorrida, numSapos));
         }
-        
-        for (Sapo sapo : sapos) {
-            sapo.start();
-        }
-        
+        try {
+            sapos.forEach(s -> s.start());
+        } catch (Exception e) {}
     }
-    
-    
-
-    
 }
--- CorridaDeSapos/Sapo.java
+++ CorridaDeSapos/Sapo.java
@@ -1,28 +1,48 @@
 package CorridaDeSapos;
+import java.util.*;
+
 
 public class Sapo extends Thread{
     private int distanciaPercorrida;
     private int distanciaPulo;
     private int id;
     private int distanciaCorrida;
+    private int numSapos;
+    public static List<Sapo> OrdemChegada = new ArrayList<Sapo>();
 
-    public Sapo(int dPercorrida, int dPulo, int id, int distanciaCorrida){
+    public Sapo(int dPercorrida, int dPulo, int id, int distanciaCorrida, int numSapos){
         distanciaPercorrida = dPercorrida;
         distanciaPulo = dPulo;
         this.id = id;
         this.distanciaCorrida = distanciaCorrida;
+        this.numSapos = numSapos;
     }
 
     public void run(){
-        System.out.println("Sapo #" + id + " começou a correr.");
-        do{
-            distanciaPercorrida += distanciaPulo;
-        }while(distanciaPercorrida < distanciaCorrida);
-        System.out.println("Sapo #" + id + " chegou.");
+        System.out.println("Sapo #" + id + " começou a correr (vel: " + distanciaPulo + "m).");
+        try{
+            do{
+                this.distanciaPercorrida += this.distanciaPulo;
+            }while(distanciaPercorrida < distanciaCorrida);
+            Chegada();
+            printColocacoes();
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void Chegada(){
+        OrdemChegada.add(this);
     }
 
     public int getDistanciaP(){
         return distanciaPercorrida;
+    }
+
+    public void printColocacoes(){
+        if(OrdemChegada.size() == numSapos){
+            OrdemChegada.forEach(e -> System.out.println("O sapos " + e.id + "# ficou em " + (OrdemChegada.indexOf(e)+1)));
+        }
     }
 
 }
